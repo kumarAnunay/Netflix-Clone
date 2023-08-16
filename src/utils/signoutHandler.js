b235c17a2c7bff0260f8d41292ae555504413231
Kumar Anunay <kumaranunay12@gmail.com>
2023-08-17 01:41:41 +0530

    Protected Route added

--- src/utils/signoutHandler.js
+++ src/utils/signoutHandler.js
@@ -5,6 +5,8 @@ export const signoutHandler = (dispatch, navigate) => {
     .signOut()
     .then(() => {
       dispatch({ type: "LOGOUT" });
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("userInfo");
       navigate("/");
     })
     .catch((error) => {
